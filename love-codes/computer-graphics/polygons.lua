4dc6c6d4627bfa40daadf2ef35a47431083113e7
osouzaTI <o.souzati@gmail.com>
2021-01-09 21:11:46 -0400

    readme

--- love-codes/computer-graphics/polygons.lua
+++ love-codes/computer-graphics/polygons.lua
@@ -6,11 +6,13 @@ local Module = {}
 function Module.fill_square(x, y, size, tbRGBA)
     local finalX = x + size
     local finalY = y + size
+
     for x = x, finalX, 1 do
         for y = y, finalY, 1 do
             draw_utils.draw_point(x, y, tbRGBA)
         end
     end
+    
 end
 
 function Module.square(x, y, size, border, tbRGBA)
@@ -34,9 +36,9 @@ function Module.square(x, y, size, border, tbRGBA)
     end
 end
 
-function Module.triangle(x1, y1, x2, y2, x3, y3, tbRGBA)   
-
+function Module.triangle(x1, y1, x2, y2, x3, y3, tbRGBA)  
     local HALF_PI = 1.57079632679 -- 90 graus
+
     local function rotateVector2D(x, y, angle)
         local xx = x * math.cos(angle) - y * math.sin(angle)
         local yy = x * math.sin(angle) + y * math.cos(angle)
@@ -46,7 +48,7 @@ function Module.triangle(x1, y1, x2, y2, x3, y3, tbRGBA)
     local function produtoEscalarVector2D(x1, y1, x2, y2)
         return x1 * x2 + y1 * y2
     end
-    
+
     local function sign(a)
         if a < 0 then
             return -1
@@ -61,6 +63,7 @@ function Module.triangle(x1, y1, x2, y2, x3, y3, tbRGBA)
 
     for xx = 0, size_screen, 1 do
         for yy = 0, size_screen, 1 do
+
             -- Criando o vetor Q em relação ao primeiro vetor u = (x2-x1, y2-y1)
             local vecQP1 = { x = x1 - xx,  y = y1 - yy }
             -- Criando o vetor Q em relação ao vetor v = (x3-x2, y3-y2)
@@ -72,15 +75,20 @@ function Module.triangle(x1, y1, x2, y2, x3, y3, tbRGBA)
             local _u = rotateVector2D(u.x, u.y, HALF_PI)
             local _v = rotateVector2D(v.x, v.y, HALF_PI)
             local _w = rotateVector2D(w.x, w.y, HALF_PI)
+
             -- Recebe o valor do produto escalar entre os vetores            
             -- acima e os vertices recebidos
             local prod1 = produtoEscalarVector2D(_u.x, _u.y, vecQP1.x , vecQP1.y)
             local prod2 = produtoEscalarVector2D(_v.x, _v.y, vecQP2.x , vecQP2.y)
             local prod3 = produtoEscalarVector2D(_w.x, _w.y, vecQP3.x , vecQP3.y)            
+            
+            -- caso o sinal do ponto Q em relação a todos os segmentos de
+            -- retas sejam iguais, o pixel sera pintado com a cor escolhida
             if sign(prod1) == sign(prod2) and sign(prod2) == sign(prod3) then
                 --print("Pinta o pixel")
                 draw_utils.draw_point(xx, yy, tbRGBA)                
             end
+
         end
     end
 end
---
+++ love-codes/computer-graphics/readme.md
@@ -0,0 +1,2 @@
+## tbRGB é uma tabela com as cores vermelho, verde e azul, nesta respectiva ordem.
+    - color blue sera representado por {r = 0, g = 0, b = 255}
